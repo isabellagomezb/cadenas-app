3f30107a351437a1200243be6b9ed7f93473451f
isabella <151967368+isabellagomezb@users.noreply.github.com>
2023-12-14 09:00:50 -0500

    Inicio

---
+++ ejercicio.py
@@ -0,0 +1,9 @@
+mensaje = "Bienvenidos a python1" 
+print(f"{mensaje.capitalize()}")
+print(f"{mensaje.upper()}")
+print(f"{mensaje.lower()}")
+print(f"{mensaje.swapcase()}")
+print(f"{mensaje.count()}")
+print(f"{len(mensaje)}")
+print(f"{len(mensaje)}")
+print(f"{len(mensaje)}")
